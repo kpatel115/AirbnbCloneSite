a83724ef9dc2d3826e06ebf574fc6ad8fdcd5e41
kpatel114 <kpatel114@hawk.iit.edu>
2023-10-26 12:24:59 -0600

    it is a complete static / responsive web page

---
+++ script.js
@@ -0,0 +1,17 @@
+/*let btn = document.getElementById("btn1");
+
+function myFunction() {
+  let newWin = window.open("about:blank", "hello", "width=200, height=200");
+  newWin.document.write("Hello World!");
+}
+
+btn.addEventListener("click", myFunction);*/
+let btn = document.getElementById("host-btn");
+
+function learnMore() {
+  let newWin = window.open("about:blank", "hello", "width=200, height=200");
+  newWin.document.write("To learn more follow this link: https://www.airbnb.com/host/homes?from_ghost=1");
+}
+let x = "string";
+let y = 5;
+let s = true;
